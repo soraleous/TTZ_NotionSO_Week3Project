245a25c5bacdba93135a74f35a5cd0a2abbe4850
BuildTools <unconfigured@null.spigotmc.org>
2021-04-11 19:00:32 +0800

    All buttons work, has tooltips, and entirely responsive as its all handled by bootstrap.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,12 +1,4 @@
-// From https://www.thatsoftwaredude.com/content/8912/create-a-basic-text-editor-in-javascript
+// Basic example of execCommand to modify the editor's text, From: https://www.thatsoftwaredude.com/content/8912/create-a-basic-text-editor-in-javascript
 function format(command, value) {
     document.execCommand(command, false, value);
-}
-
-function setUrl() {
-    var url = document.getElementById('txtUrl').value;
-    var sText = document.getSelection();
-
-    document.execCommand('insertHTML', false, '<a href="' + url + '" target="_blank">' + sText + '</a>');
-    format('createlink', url);
 }
